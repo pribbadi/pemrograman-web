af84bf1d803b0a2cd070ccf61a86d0ad2deb22b8
pribbadi <pribbadi@users.noreply.github.com>
2018-10-01 19:19:05 +0700

    kuis untuk MI5
    soal ini dapat dikerjakan sejak soal ini dirilis/diupload

---
+++ 4. Tugas Harian/Kuis MI6/binary search.php
@@ -0,0 +1,36 @@
+<?php 
+//nim 109533423206 ==> 423206
+//data yang masuk array = 6 digit terakhir
+$a = array(4,2,3,2,0,6); //6 digit terakhir dari nim
+$c=count($a);
+$b = false;
+while ($b === false) {
+    $b = true;
+    for ($i = 0; $i < $c-1; ++$i) {
+        $x = $a[$i];
+        $y = $a[$i+1];
+        if ($y < $x) {
+            $a[$i] = $y;
+            $a[$i+1] = $x;
+            $b = false;
+        }
+    }
+}
+foreach ($a as $value) {
+    echo $value;
+}
+$list=$a;
+$val = $list[0]; //isi $val adalah 1) jika nim genap = $list[0] dan 2) jika nim ganjil = $list[5]
+$low = 0;
+$high = $c - 1;
+while ($low <= $high) {
+    $mid = floor(($low + $high) / 2);
+    if ($list[$mid] == $val) {
+        echo "<br>Data $val ditemukan di : $ a[$mid] pada array setelah pengurutan";
+        break;
+    } else if ($list[$mid] > $val) {
+        $high = $mid - 1;
+    } else if ($list[$mid] < $val) {
+        $low = $mid + 1;
+    }
+}
